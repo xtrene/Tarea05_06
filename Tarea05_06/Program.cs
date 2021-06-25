aa10bb9a126cbeef28c2b08e20848b316a907f26
Carlos <Carlos@xtrene.com>
2021-06-25 17:18:40 +0200

    Agregar archivos de proyecto.

---
+++ Tarea05_06/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Tarea05_06
+{
+    class Program
+    {
+        static int cifras(int numero)
+        {
+            int resultado = 0;
+            while (numero > 0)
+            {
+                numero = numero / 10;
+                resultado++;
+            }
+
+            return resultado;
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Introduce un número entero positivo: ");
+            int entrada = Convert.ToInt32(Console.ReadLine());
+            
+            if (entrada >= 0)
+            {
+                int numeroCifras = cifras(entrada);
+                Console.WriteLine("El número {0} tiene {1} cifra(s).",entrada,numeroCifras);
+            }
+            else
+            {
+                Console.WriteLine("ERROR .... sólo número enteros positivos");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
